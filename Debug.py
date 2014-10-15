b4564785e305a298065934e7b198acbd76a8b09a
lochnessduck <son.kindle@gmail.com>
2014-10-15 15:22:49 -0700

    Finally found and fixed problem
    This is a scaled back version. I deleted a lot of advanced functions
    until I could more easily troubleshoot my problems. I also created a
    class BoundingBox to help standardize creating coordinates everywhere.
    But that wasn't the error. I found that finding the box that represented
    changes in the difference image was wrong. 1) it used "if True in
    topSlice:" which does not return True if there are positive values in
    the slice. It appears that it only returns True when a majority of
    values are positive. 2) bottom and right coordinates are off by one
    pixel. This means small, 1x5 images (or similar) are actually
    interpreted as 0x4, creating an empty image that then causes errors past
    that. Both errors have been corrected. 1) I used if sum(topSlice): 2) if
    coordinate contains a positive pixel, I add +1 to coordinate and break
    from for-loop. I hope to re-implement the advanced functions again.

--- Debug.py
+++ Debug.py
@@ -1,9 +1,9 @@
+from __future__ import print_function
 import PIL
 from PIL import ImageChops, ImageGrab
 from DisplayCanvas import DisplayImage, Display, BoundingBox
 import time
 import numpy as np
-import Debug
 
 class ScreenMonitor:
 
@@ -12,6 +12,7 @@ class ScreenMonitor:
             bbox = BoundingBox(0, 0, 1024, 768)  # supposed to be full screen size
         self.image = None  # ImageGrab.grab(bbox)  first instance will not have anything displayed
         self.bbox = bbox
+        self.debug = True  # user must manually set this later
 
     def get_changes_as_display_images(self, totalRefresh=False):
         newIm = ImageGrab.grab(self.bbox)
@@ -25,7 +26,12 @@ class ScreenMonitor:
     def get_cropped_images_update(self, imOld, imNew):
         imdif = ImageChops.difference(imOld, imNew)
         difBW = imdif.convert('L')
+        self.difference = difBW
         difBW.load()
+        # if self.debug:
+            ## return [DisplayImage(imNew, (0,0))]
+            # debugImg = difBW.convert("RGB")  # always requires an rgb image
+            # return [DisplayImage(debugImg, (0,0))]  # debuggin returns image difference to display
         imArray = np.asarray(difBW, dtype='int32')
         bbox = self.create_square_from_image(imArray)
         if bbox is None or bbox.size == (0,0):  # if bbox is None or box has no size
@@ -44,7 +50,6 @@ class ScreenMonitor:
         for y in range(im.shape[0] - 1, top - 1, -1):
             bottomSlice = im[y, :]
             if sum(bottomSlice):
-                y += 1  # if we found something, then that means the bounding box must be extended by one downward. So that the bounding box correctly shows the bottom part. Remember, this is where bottom -1 is included, but not bottom coordinate.
                 break
         bottom = y
         for x in range(im.shape[1]):
@@ -55,32 +60,15 @@ class ScreenMonitor:
         for x in range(im.shape[1] - 1, left - 1, -1):
             rightSlice = im[:, x]
             if sum(rightSlice):
-                x += 1  # if we found something, then that means the bounding box to the right must extend by one (because x is not included in the image, x-1 IS)
                 break
         right = x
         bbox = BoundingBox(left, top, right, bottom)
+        print(bbox)
         return bbox  # top-left to bottom-right coordinates (in x, y format)
 
-
-class DisplayUpdater:
-
-    def __init__(self, display, monitor):
-        self.display = display
-        self.monitor = monitor
-
-    def update(self):
-        dImages = self.monitor.get_changes_as_display_images()
-        self.display.extend(dImages)
-
-
-if __name__ == '__main__':
-    bbox = BoundingBox(0, 0, 300, 600)
-    display = Display(bbox.size)
-    monitor = ScreenMonitor(bbox)
-    updater = DisplayUpdater(display, monitor)
-    while display.tick():
-        time.sleep(1)
-        updater.update()
-    
-    
-    
+# if True:
+	# imgs = monitor.get_changes_as_display_images()
+	# display.extend(imgs)
+	# display.tick()
+	# monitor.image.save('aaa_img.png')
+	# monitor.difference.save('aaa_img_dif.png')
--- DisplayCanvas.py
+++ DisplayCanvas.py
@@ -10,23 +10,18 @@ class Display:
     def __init__(self, size=(700, 400)):
         pygame.init()
         self.screen = pygame.display.set_mode(size)
-        self.images = []  #pygame.sprite.group() # just call .draw to draw all
+        self.surfaces = []  #pygame.sprite.group() # just call .draw to draw all
         pygame.display.set_caption("Connected to...")
-        # Used to manage how fast the screen updates
-        self.clock = pygame.time.Clock()        
+        self.clock = pygame.time.Clock()  # Used to manage how fast the screen updates
         self.exit = False
         self.size = size
-        self._dirtyboxOutdated = False
-        self._create_dirtybox()  # a matrix which determines invisible images
-        
+
     def append(self, image):
         """ requires a display image, which includes coordinates of where to
             display image
         """
-        self.images.append(image)
-        self._update_dirtybox()
-        self._remove_invisible_images()
-    
+        self.surfaces.append(image)
+
     def extend(self, images):
         """ requires iterable of display images
         """
@@ -34,9 +29,8 @@ class Display:
             self.append(im)
         
     def draw(self):
-        #self.images.draw(self.screen)
-        for image in self.images:
-            image.draw(self.screen)
+        for dImage in self.surfaces:  #displayImage
+            dImage.draw(self.screen)            
         pygame.display.flip()
         
     def tick(self):
@@ -68,50 +62,26 @@ class Display:
         pygame.quit()
         
     def __iter__(self):
-        for dim in self.images:
+        for dim in self.surfaces:
             yield dim
             
     def __getitem__(self, index):
-        return self.images[index]
+        return self.surfaces[index]
     
     def __setitem__(self, index, im):
-        self._dirtyboxOutdated = True
-        self.images[index] = im
+        self.surfaces[index] = im
         
     def __nonzero__(self):
-        return len(self.images)
+        return len(self.surfaces)
         
     def __contains__(self, im):
-        return im in self.images
+        return im in self.surfaces
     
     def __delitem__(self, index):
-        self._dirtyboxOutdated = True
-        del self.images[index]
+        del self.surfaces[index]
 
     def __len__(self):
-        return len(self.images)
-
-    def _create_dirtybox(self):
-        self._dirtybox = np.zeros(self.size) - 1
-        for i, im in enumerate(self):
-            self._dirtybox[im.xCoordinates, im.yCoordinates] = i
-        self._dirtyboxOutdated = False
-
-    def _update_dirtybox(self):
-        """ call after adding an image to the canvas
-        """
-        i = len(self) - 1
-        im = self[i]
-        self._dirtybox[im.xCoordinates, im.yCoordinates] = i
-
-    def _remove_invisible_images(self):
-        if self._dirtyboxOutdated:
-            self._create_dirtybox()
-        for i in range(len(self) - 1, -1, -1):
-            if i not in self._dirtybox:
-                del self[i]  # if image is NOT visible, delete the image
-                # this also tells dirtybox that it's no longer valid.
-                
+        return len(self.surfaces)
         
 class DisplayImage(pygame.sprite.Sprite):
     
@@ -119,44 +89,56 @@ class DisplayImage(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)   # call parent. use Super()instead?
         self.id = 19  # eventually needs to create separate IDs
         imString = im.tostring("raw", "RGB")  # a PILLOW function, default encoder is "raw"
-        # tostring will eventually be replaced by tobytes
-        #imString = im.convert("RGBA").tostring(...)  # this works as well. maybe more robust
         self.size = im.size
-        self.image = pygame.image.fromstring(imString, self.size, "RGB")
+        self.surface = pygame.image.fromstring(imString, self.size, "RGB")
+        self.surface.convert()  # loads / converts image for much faster operation
         self.xy = xy
-        self.bbox = (xy[0], xy[1], xy[0] + self.size[0], xy[1] + self.size[1])
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = xy
-        self.yCoordinates = slice(xy[1], xy[1] + self.size[1])  # get y coordinates that this image occupies
-        self.xCoordinates = slice(xy[0], xy[0] + self.size[0])  # get x coordinates that this image occupies
+        self.rect = pygame.Rect(xy, im.size)  # lots of useful function in pygame.Rect
         
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+    @classmethod
+    def from_string(cls, imString, imSize, xy=(0,0)):
+        im = pygame.image.fromstring(imString, imSize, "RGB")
+        return cls(im, xy)
     
     @classmethod
     def fromScreenshot(cls, bbox):  # bbox coordinates are top-left to bottom-right
         im = ImageGrab.grab(bbox)
         return cls(im, bbox[:2])
-        #return cls(im, bbox[:2])
 
+    def draw(self, screen):
+        screen.blit(self.surface, self.rect)
+        
+class BoundingBox:
 
+    def __init__(self, left, top, right, bottom):
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.w = right - left
+        self.width = self.w
+        self.h = bottom - top
+        self.height = self.h
+        self.size = (self.w, self.h)
+        self.xy = (left, top)
+        self.rect = pygame.Rect(self.xy, self.size)
+        
+    def __iter__(self):
+        for coordinate in [self.left, self.top, self.right, self.bottom]:
+            yield coordinate
+    
+    def __repr__(self):
+        return '(' + str(self.left) + ', ' + str(self.top) + ', ' + str(self.right) + ', ' + str(self.bottom) + ')'
+
+            
  
 if __name__ == '__main__':
     size = (700, 400)
     display = Display(size)
-    #im = Image.open('asdf.png')
-    imString = '\x00\x00\x00\x00\x00\x00f\xb6\xff\xff\xff\xff\xff\xff\xb6\xff\xff\xff\xff\xdb\x90:\x00:\x90\xdb\xff\xff\xff\xff\xff\xff\xff\xff\xff\xdb\x90:\x00f\xb6\xff\xff\xff\xff\xff\xff\xff\xff\xb6f\x00\x00:\x90\xdb\xff\xff\xff\xff'
-    im = Image.fromstring('RGB',(5,4), imString) 
-    dim = DisplayImage(im, (0,0))
-    dim2 = DisplayImage(im, (20, 20))
-    display.append(dim)
-    display.append(dim2)
-    del display[0] # remove first image added
-    for x in range(15, 30, 3):
-        for y in range(15, 30, 3):
+    im = ImageGrab.grab((0,0,50,40))
+    for x in range(150, 300, 30):
+        for y in range(150, 300, 30):
             dim = DisplayImage(im, (x, y))
             display.append(dim)
-    print('should print 25:', len(display))  # should print 25, because we the 2nd image manually
-                        # added will be auto-removed once it's invisible
     while display.tick():
         pass  # when display.tick() returns false that means it has quit
